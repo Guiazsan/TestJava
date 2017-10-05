5f0d8beba35e3dc456dae7aef878f502c4173b1b
HP <HP@172.21.151.249>
2017-10-05 20:22:24 -0300

    Criação do Teste

---
+++ src/gitjava/GitJava.java
@@ -0,0 +1,12 @@
+package gitjava;
+import java.util.Scanner;
+
+public class GitJava {
+
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+    }
+
+}
